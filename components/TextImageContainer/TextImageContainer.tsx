21ae0ce70d2d4de17a91feeaf54745de8741d0e1
UzhNaSkovorodke <tukalov.kirill99@gmail.com>
2023-01-05 14:26:35 +0700

    coninue-landing-design

--- components/TextImageContainer/TextImageContainer.tsx
+++ components/TextImageContainer/TextImageContainer.tsx
@@ -13,15 +13,20 @@ const TextImageContainer: React.FunctionComponent<ITextImageContainerProps> = ({
 }) => {
 	return (
 		<>
-			{type === 'imgtext' ? (
+			{type === 'textimg' ? (
 				<div className="flex flex-wrap justify-between items-center py-16">
-					<div className="w-1/2 text-2xl font-normal">{text}</div>
+					<div className="w-1/2 text-base tablet:text-base laptop:text-lg desktop:text-lg font-normal">
+						{text}
+					</div>
+
 					<div className="w-1/2">{image}</div>
 				</div>
 			) : (
 				<div className="flex flex-wrap justify-between items-center py-16">
 					<div className="w-1/2">{image}</div>
-					<div className="w-1/2 text-2xl font-normal">{text}</div>
+					<div className="w-1/2 text-base tablet:text-base laptop:text-lg desktop:text-lg font-normal">
+						{text}
+					</div>
 				</div>
 			)}
 		</>
